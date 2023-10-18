ced7471c641e935547c366b669f4cf768accb7e9
luwi987 <146705464+luwi987@users.noreply.github.com>
2023-10-18 14:29:52 +0200

    Update StorageHelper.cs

--- ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
@@ -21,7 +21,7 @@ namespace ImageResizeWebApp.Helpers
                 return true;
             }
 
-            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg" };
+            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg", ".xlsx" };
 
             return formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
         }
