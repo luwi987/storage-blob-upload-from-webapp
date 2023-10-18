7dab021ce0d7de8d4b0f9357db63caa56750c3ca
luwi987 <146705464+luwi987@users.noreply.github.com>
2023-10-18 15:58:50 +0200

    Update ImagesController.cs

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -40,7 +40,7 @@ namespace ImageResizeWebApp.Controllers
 
                 foreach (var formFile in files)
                 {
-                    if (StorageHelper.IsImage(formFile))
+                    if (StorageHelper.IsValidFile(formFile))
                     {
                         if (formFile.Length > 0)
                         {
