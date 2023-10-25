b730a28abcd2642d7e56516e7439360251931184
luwi987 <146705464+luwi987@users.noreply.github.com>
2023-10-25 14:20:39 +0200

    Update StorageHelper.cs

--- ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
@@ -15,6 +15,8 @@ namespace ImageResizeWebApp.Helpers
 {
     public static class StorageHelper
     {
+        private static readonly string[] ImageFormats = { ".jpg", ".png", ".gif", ".jpeg" };
+
         public static bool IsValidFile(IFormFile file)
         {
             if (file.ContentType.Contains("image"))
@@ -22,21 +24,13 @@ namespace ImageResizeWebApp.Helpers
                 return true;
             }
 
-            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg", ".xlsx" };
-
-            return formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
+            return ImageFormats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
         }
 
-        public static async Task<bool> UploadFileToStorage(Stream fileStream, string fileName,
-                                                            AzureStorageConfig _storageConfig)
+        public static async Task<bool> UploadFileToStorage(Stream fileStream, string fileName, AzureStorageConfig _storageConfig)
         {
-            // Überprüfen, ob es sich um ein Bild handelt
-            if (fileName.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
-                fileName.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
-                fileName.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
-                fileName.EndsWith(".gif", StringComparison.OrdinalIgnoreCase))
+            if (IsImage(fileName))
             {
-                // Verwenden Sie ImageSharp, um das Bild zu laden und zu skalieren
                 using var image = Image.Load(fileStream);
                 image.Mutate(x => x.Resize(new ResizeOptions
                 {
@@ -44,56 +38,23 @@ namespace ImageResizeWebApp.Helpers
                     Mode = ResizeMode.Max
                 }));
 
-                // Konvertieren Sie das skalierte Bild zurück in einen Stream
-                var memoryStream = new MemoryStream();
-                image.Save(memoryStream, new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder());
-                memoryStream.Position = 0;
-                fileStream = memoryStream;
+                fileStream.Position = 0;
+                image.Save(fileStream, new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder());
+                fileStream.Position = 0;
             }
 
-            // Create a URI to the blob 
-            Uri blobUri = new Uri("https://" +
-                                  _storageConfig.AccountName +
-                                  ".blob.core.windows.net/" +
-                                  _storageConfig.ImageContainer +
-                                  "/" + fileName);
-
-            // Create StorageSharedKeyCredentials object by reading
-            // the values from the configuration (appsettings.json)
-            StorageSharedKeyCredential storageCredentials =
-                new StorageSharedKeyCredential(_storageConfig.AccountName, _storageConfig.AccountKey);
-
-            // Create the blob client.
+            Uri blobUri = new Uri($"https://{_storageConfig.AccountName}.blob.core.windows.net/{_storageConfig.ImageContainer}/{fileName}");
+            StorageSharedKeyCredential storageCredentials = new StorageSharedKeyCredential(_storageConfig.AccountName, _storageConfig.AccountKey);
             BlobClient blobClient = new BlobClient(blobUri, storageCredentials);
 
-            // Upload the file with overwrite option set to true
             await blobClient.UploadAsync(fileStream, true);
 
-            return await Task.FromResult(true);
+            return true;
         }
 
-        public static async Task<List<string>> GetThumbNailUrls(AzureStorageConfig _storageConfig)
+        private static bool IsImage(string fileName)
         {
-            List<string> thumbnailUrls = new List<string>();
-
-            // Create a URI to the storage account
-            Uri accountUri = new Uri("https://" + _storageConfig.AccountName + ".blob.core.windows.net/");
-
-            // Create BlobServiceClient from the account URI
-            BlobServiceClient blobServiceClient = new BlobServiceClient(accountUri);
-
-            // Get reference to the container
-            BlobContainerClient container = blobServiceClient.GetBlobContainerClient(_storageConfig.ThumbnailContainer);
-
-            if (container.Exists())
-            {
-                foreach (BlobItem blobItem in container.GetBlobs())
-                {
-                    thumbnailUrls.Add(container.Uri + "/" + blobItem.Name);
-                }
-            }
-
-            return await Task.FromResult(thumbnailUrls);
+            return ImageFormats.Any(item => fileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
