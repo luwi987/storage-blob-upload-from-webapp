d38b8d64e7bca76b2ce1732414b1aa5e8415e608
luwi987 <146705464+luwi987@users.noreply.github.com>
2023-11-16 09:49:55 +0100

    Update HomeController.cs

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/HomeController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/HomeController.cs
@@ -5,14 +5,21 @@ namespace ImageResizeWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home/Index
         public IActionResult Index()
         {
+            // Return the default view for the Index action.
             return View();
         }
 
+        // GET: Home/Error
         public IActionResult Error()
         {
+            // The ViewData dictionary is a dynamic object that provides a convenient way to pass data from a controller to a view.
+            // Here, it's used to pass the Request ID in case of an error for diagnostic purposes.
             ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+            // Return the view for the Error action.
             return View();
         }
     }
